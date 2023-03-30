ec0df6f43cc3e256a53ed86f309035caddfee17b
jay savani <jaysavani604@gmail.com>
2023-03-30 15:57:43 +0200

    Add regression: tts, fit, predict, load methods

--- HyperXgboost.py
+++ HyperXgboost.py
@@ -1,26 +1,105 @@
 import pickle
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error, mean_absolute_error
 from xgboost import XGBRegressor
+import optuna
+from optuna.distributions import IntDistribution ,FloatDistribution, CategoricalDistribution
+from optuna.integration import XGBoostPruningCallback, OptunaSearchCV 
 
 
 class HyperXGBoost:
     
-    def __init__(self, model_params: dict = {}):
+    _DEFAULT_TASKS = ['regression', 'classification']
+    
+    def __init__(self, task:str = 'regression', model_params: dict = {}):
+         
+        self.model_params_ = model_params
+        self.task_ = task if task in _DEFAULT_TASKS else raise ValueError(f"Mentioned {self.task_} is not supported !!! Choose either 'regression' or 'classification'.")
+    
+    def load_data(self, file_path:str, file_type:str = 'csv', **kwargs):
+        #self.data_ = pd.read_csv(file_path, **kwargs)
+        pass
+    
+    def train_test_split_Xy(self, X,y, test_size = 0.3, random_state = 42,**kwargs):
+        X_train, X_test, y_train, y_test = train_test_split(X = X, y=y, test_size=test_size, **kwargs)
+        return X_train, X_test, y_train, y_test
+    
+    def fit(self, X_train, y_train, X_test, y_test, study_n_trials = 100, early_stop_rounds = 10,  params= dict()):
         
-        self.model_params = model_params
+        if self.task_ == 'regression':
+            def objective(trial):
+                
+                param = {
+                    'max_depth': trial.suggest_int('max_depth', 1, 20),
+                    'learning_rate': trial.suggest_float('learning_rate', 0.001, 1.0),
+                    'n_estimators': trial.suggest_int('n_estimators', 50, 1000),
+                    'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+                    'gamma': trial.suggest_float('gamma', 0.01, 1.0),
+                    'subsample': trial.suggest_float('subsample', 0.01, 1.0),
+                    'colsample_bytree': trial.suggest_float('colsample_bytree', 0.01, 1.0),
+                    'reg_alpha': trial.suggest_float('reg_alpha', 0.01, 1.0),
+                    'reg_lambda': trial.suggest_float('reg_lambda', 0.01, 1.0),
+                    'random_state': trial.suggest_int('random_state', 1, 1000)
+                }
+                
+                if model_params:
+                    params.update(model_params)
+                
+                model = xgboost.XGBRegressor(**param)
+                model.fit(X_train, y_train, early_stopping_rounds=early_stop_rounds)
+                y_pred = model.predict(X_test)
+                return mean_squared_error(y_test, y_pred)
+            
+            study = optuna.create_study(direction='minimize', study_name='regression')
+            study.optimize(objective, n_trials = study_n_trials)
+            
+            self.study_ = study
+            self.best_params_ = study.best_params
+            self.best_value_ = study.best_value
+            self.best_trial_ = study.best_trial
+            
+            return self.study_
+        
+        elif self.task == 'classification':
+            pass
+        
+        else:
+            raise ImplementedError(f"""
+                Mentioned {self.task_} is not supported !!! Choose either 'regression' or 'classification'.
+            """)
+                
     
-    def fit(self,):
-        pass
+    def predict(self, X_test, y_test, metrics_summary: bool = True):
+        
+        if self.task_ == 'regression':
+            tuned_model = xgboost.XGBRegressor(**study.best_params)
+        elif:
+            pass
+        tuned_model.fit(X_train, y_train)
+        y_pred = tuned_model.predict(X_test)
+
+        if metrics_summary:
+            if self.task_ == 'regression':
+                print(f'MAE: {mean_absolute_error(y_test, y_pred)}')
+                print(f'MSE: {mean_squared_error(y_test, y_pred)}')
+                print(f'RMSE: {np.sqrt(mean_squared_error(y_test, y_pred))}')
+            
+            elif self.task_ == 'classfication':
+                pass
+            
+            else:
+                raise ImplementedError(f"""
+                    Mentioned {self.task_} is not supported !!! Choose either 'regression' or 'classification'.
+                """)
+
     
-    def predict(self,):
-        pass
-    
-    def save(self,file_path):
-        output_file = open(f"{file_path}xgboost_model.pickle", "w")
+    def save(self, model, file_path):
+        output_file = open(f"{file_path}/{model}_{self.task_}.pickle", "w")
         pickle.dump(self,output_file)
     
     @classmethod
-    def load(cls, file_path):
-        input_file = open(f"{file_path}xgboost_model.pickle", "rb")
+    def load(cls, model, file_path):
+        input_file = open(f"{file_path}{model}_{self.task_}.pickle", "rb")
         return pickle.load(input_file)
